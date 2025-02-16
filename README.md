c2465540b86b4ec781da4b71f983833b04b90a6c
Tommy Tran <tommytt721@gmail.com>
2025-02-16 21:44:00 +0100

    readme progress

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nixos
+
+## nixos-anywhere on azure
+
+1. setup azure linux vm using terraform/vm
+2. clone 
+3. edit disk-config, ssh public key to configuration.nix
+4. nix run nixpkgs#nixos-anywhere -- --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <usesr>@<ip>
--- terraform/vm/main.tf
+++ terraform/vm/main.tf
@@ -81,7 +81,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  size                = "Standard_B1s"
+  size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.example.id,
